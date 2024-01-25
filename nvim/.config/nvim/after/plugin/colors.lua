23b9eb43c4a42106574333bcf152ff9f221e5e21
Eric Johnson <ericmjohnson1991@msn.com>
2024-01-25 13:03:51 -0800

    macchiato flavor for color

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,6 +1,6 @@
 
 function ColorMyPencils(color)
-	color = color or "catppuccin"
+	color = color or "catppuccin-macchiato"
 	vim.cmd.colorscheme(color)
     vim.api.nvim_set_hl(0, 'LineNr', { fg = "white"} )
 end
