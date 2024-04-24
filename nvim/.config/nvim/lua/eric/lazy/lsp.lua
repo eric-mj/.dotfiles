2ae395012d37f439465f901e7d3a3452a68221ac
Eric Johnson <ericmjohnson1991@msn.com>
2024-04-24 16:33:45 -0700

    Fix some issues with the migration. Harpoon still broken.

--- nvim/.config/nvim/lua/eric/lazy/lsp.lua
+++ nvim/.config/nvim/lua/eric/lazy/lsp.lua
@@ -1,4 +1,4 @@
-    return {
+return {
     "neovim/nvim-lspconfig",
     dependencies = {
         "williamboman/mason.nvim",
@@ -32,7 +32,6 @@
             },
             handlers = {
                 function(server_name) -- default handler (optional)
-
                     require("lspconfig")[server_name].setup {
                         capabilities = capabilities
                     }
@@ -44,7 +43,7 @@
                         capabilities = capabilities,
                         settings = {
                             Lua = {
-				    runtime = { version = "Lua 5.1" },
+                                runtime = { version = "Lua 5.1" },
                                 diagnostics = {
                                     globals = { "vim", "it", "describe", "before_each", "after_each" },
                                 }
