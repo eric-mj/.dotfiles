253705febdeb5b979d515b4bdf4801bea8db2ed9
Eric Johnson <ejohnson@onesignal.com>
2024-08-05 13:38:38 -0700

    Make snippets properly jumpable

--- nvim/.config/nvim/lua/eric/lazy/lsp.lua
+++ nvim/.config/nvim/lua/eric/lazy/lsp.lua
@@ -81,9 +81,9 @@ return {
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 { name = 'luasnip' }, -- For luasnip users.
-            }, {
                 { name = 'buffer' },
-            })
+            }
+            )
         })
 
         vim.diagnostic.config({
@@ -97,6 +97,38 @@ return {
                 prefix = "",
             },
         })
+        local ls = require "luasnip"
+        vim.snippet.expand = ls.lsp_expand
+
+        vim.snippet.active = function(filter)
+            filter = filter or {}
+            filter.direction = filter.direction or 1
+
+            if filter.direction == 1 then
+                return ls.expand_or_jumpable()
+            else
+                return ls.jumpable(filter.direction)
+            end
+        end
+
+        vim.snippet.jump = function(direction)
+            if direction == 1 then
+                if ls.expandable() then
+                    return ls.expand_or_jump()
+                else
+                    return ls.jumpable(1) and ls.jump(1)
+                end
+            else
+                return ls.jumpable(-1) and ls.jump(-1)
+            end
+        end
+
+        vim.keymap.set({ "i", "s" }, "<c-k>", function()
+            return vim.snippet.active { direction = 1 } and vim.snippet.jump(1)
+        end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<c-j>", function()
+            return vim.snippet.active { direction = -1 } and vim.snippet.jump(-1)
+        end, { silent = true })
         vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
     end
 }
