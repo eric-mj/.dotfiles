98479d5ce39e689a8583d5077d5a8667db0ed685
Eric Johnson <ejohnson@onesignal.com>
2024-08-05 13:38:13 -0700

    Fix mini so it's actually setup

--- nvim/.config/nvim/lua/eric/lazy/mini.lua
+++ nvim/.config/nvim/lua/eric/lazy/mini.lua
@@ -1,1 +1,8 @@
-return { 'echasnovski/mini.nvim', version = '*' }
+return {
+    'echasnovski/mini.nvim',
+    version = '*',
+    config = function()
+        require('mini.surround').setup()
+        require('mini.ai').setup()
+    end
+}
