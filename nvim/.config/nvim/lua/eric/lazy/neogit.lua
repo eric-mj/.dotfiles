0dd3dbe4dbb23bd76a7cbc5e65e11b5b4a0c6b5c
Eric Johnson <ejohnson@onesignal.com>
2024-08-06 16:50:29 -0700

    Update git workflow

--- nvim/.config/nvim/lua/eric/lazy/neogit.lua
+++ nvim/.config/nvim/lua/eric/lazy/neogit.lua
@@ -1,11 +1,36 @@
 return {
     "NeogitOrg/neogit",
     dependencies = {
-        "nvim-lua/plenary.nvim", -- required
+        "nvim-lua/plenary.nvim",  -- required
         "sindrets/diffview.nvim", -- optional - Diff integration
 
         -- Only one of these is needed, not both.
         "nvim-telescope/telescope.nvim", -- optional
     },
-    config = true
+
+    config = function()
+        local neogit = require('neogit')
+        neogit.setup()
+
+        vim.keymap.set("n", "<leader>gs", neogit.open,
+            { silent = true, noremap = true }
+        )
+
+        vim.keymap.set("n", "<leader>gc", ":Neogit commit<CR>",
+            { silent = true, noremap = true }
+        )
+
+        vim.keymap.set("n", "<leader>gp", ":Neogit pull <CR>",
+            { silent = true, noremap = true }
+        )
+
+        vim.keymap.set("n", "<leader>gP", ":Neogit push<CR>",
+            { silent = true, noremap = true }
+        )
+
+        vim.keymap.set("n", "<leader>gb", ":Telescope git_branches<CR>",
+            { silent = true, noremap = true }
+        )
+    end
+
 }
